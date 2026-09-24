20900679fe8e4ad1f7787e56f54987b88f457829
Yuto <msc_prop@proton.me>
2026-09-24 23:45:59 +0900

    test: restore qualified module compiler before import witness

--- bridge/module_compiler.mjs
+++ bridge/module_compiler.mjs
@@ -3,6 +3,5 @@ import { assertCompatibleModule } from "./internal.mjs";
 export async function compileFlowModule(source) {
   const module = await WebAssembly.compileStreaming(source);
   assertCompatibleModule(module);
-  new WebAssembly.Instance(module, {});
   return module;
 }
