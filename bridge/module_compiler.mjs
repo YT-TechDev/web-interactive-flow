d7f0edc63c52088811c9d3d1621dc71a3eca825a
Yuto <msc_prop@proton.me>
2026-09-24 23:48:08 +0900

    test(mutant): make compiler own resource fetch

--- bridge/module_compiler.mjs
+++ bridge/module_compiler.mjs
@@ -1,7 +1,8 @@
 import { assertCompatibleModule } from "./internal.mjs";
 
 export async function compileFlowModule(source) {
-  const module = await WebAssembly.compileStreaming(source);
+  const response = await fetch(source);
+  const module = await WebAssembly.compileStreaming(response);
   assertCompatibleModule(module);
   return module;
 }
