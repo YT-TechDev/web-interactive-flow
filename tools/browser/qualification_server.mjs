990973af7eef5fd675208c5a83d8e1dfe15e1f6a
Yuto <msc_prop@proton.me>
2026-09-25 00:24:43 +0900

    test: restore qualification Wasm MIME

--- tools/browser/qualification_server.mjs
+++ tools/browser/qualification_server.mjs
@@ -98,7 +98,7 @@ const ROUTES = new Map([
   [
     "/core.wasm",
     {
-      contentType: "application/octet-stream",
+      contentType: "application/wasm",
       body: async () =>
         readFile(new URL("../../_build/wasm/debug/build/core/core.wasm", import.meta.url)),
     },
