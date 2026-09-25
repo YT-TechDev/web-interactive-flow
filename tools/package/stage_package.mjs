00a929a9c4860825293390548240388309936ffb
Yuto <msc_prop@proton.me>
2026-09-25 18:32:04 +0900

    test(mutant): couple root package facade to R3F

--- tools/package/stage_package.mjs
+++ tools/package/stage_package.mjs
@@ -23,6 +23,7 @@ const ROOT_FACADE = `export { createFlowRuntime } from "./bridge/runtime.mjs";
 export { compileFlowModule } from "./bridge/module_compiler.mjs";
 export { createFrameScheduler } from "./bridge/frame_scheduler.mjs";
 export { applyWheelNavigationIntent } from "./bridge/wheel_ownership.mjs";
+export { useFlowFrame } from "./adapters/r3f/use_flow_frame.mjs";
 `;
 
 const R3F_FACADE =
